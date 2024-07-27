4092cd905dc4cfb887b94785b4fc685b9c3a9628
jjegarcia <jjegarcia@hotmail.com>
2024-07-27 10:08:53 +0100

    added handle for task1

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,14 @@
 int c1 = 0;
 int c2 = 0;
 
+TaskHandle_t task1_handle = NULL;
+TaskHandle_t task2_handle = NULL;
+
 void task1(void *parameters)
 {
   for (;;)
   {
-    Serial.print("Task 1 Counter:");
+    Serial.print("Task 1 Counter1:");
     Serial.println(c1++);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
@@ -16,7 +19,7 @@ void task2(void *parameters)
 {
   for (;;)
   {
-    Serial.print("Task 2 Counter:");
+    Serial.print("Task 2 Counter2:");
     Serial.println(c2++);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
@@ -24,11 +27,16 @@ void task2(void *parameters)
 void setup()
 {
   Serial.begin(115200);
-  xTaskCreate(task1, "Task1", 1000, NULL, 1, NULL);
-  xTaskCreate(task2, "Task2", 1000, NULL, 1, NULL);
+  xTaskCreate(task1, "Task1", 1000, NULL, 1, &task1_handle);
+  xTaskCreate(task2, "Task2", 1000, NULL, 2, &task2_handle);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  if (c1 > 3 && task1_handle != NULL)
+  {
+    // Serial.print("KK");
+    vTaskSuspend(task1_handle);
+  }
+  
 }
